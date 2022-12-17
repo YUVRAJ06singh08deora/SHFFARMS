35820e0534c26cb8e2582ad3cc731dcd897d5279
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2022-12-18 00:50:01 +0530

    Intro Activity Created and Main Activity Started

---
+++ app/src/main/java/com/example/shffarms/Activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shffarms.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.shffarms.R;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
